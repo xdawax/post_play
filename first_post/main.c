296bf692b0e2eaf14a4b34f1827adcf8e655abf2
xdawax <david.carlsson.6652@student.uu.se>
2018-08-20 22:56:13 +0200

    Add transmission of packet via HTTP POST

--- first_post/main.c
+++ first_post/main.c
@@ -1,50 +1,43 @@
 #include <stdio.h>
-#include <curl/curl.h>
-#include <json/json.h>
+#include <unistd.h>
+#include "pkt_to_json.h"
 
-void start_curl(json_object *json_park);
+#define TARGET_ADDRESS 'g'
+#define SENDER_ADDRESS '1'
+#define TYPE 8
+#define SEQ 100
+#define PDS 1
+#define PAYLOAD 'y'
 
 int main(void)
 {
 
-    printf("hello!\n");
+    char PD[1] = {PAYLOAD};
+    int counter = 0;
 
-    //char *parking = "{\"key\":\"carIsParking\"}";
-    char *parking = "{\"key\":\"carIsLeaving\"}";
-    json_object *json_park = json_tokener_parse(parking);
+    pkt_t *pkt = pkt_create(TARGET_ADDRESS, SENDER_ADDRESS, TYPE, SEQ, PDS, PD);
 
-    printf("%s\n", json_object_get_string(json_park));
 
-    start_curl(json_park);
+    while (true) {
+
+        if (counter % 4 == 0) {
+            *(pkt->pd) = 'y';
+        } else if (counter % 2 == 0) {
+            *(pkt->pd) = 'n';
+        }else if (counter % 5 == 0) {
+            pkt->sadr = '1';
+        } else if (counter % 3 == 0) {
+            pkt->sadr = '2';
+        }
+        transmit_pkt_as_json (pkt);
+        sleep(2);
+        counter++;
+    }
+
+
+    pkt_free(pkt);
 
     return 0;
+
 }
 
-void start_curl(json_object *json_park) {
-    CURL *curl;
-    CURLcode res;
-
-    /* In windows, this will init the winsock stuff */
-    curl_global_init(CURL_GLOBAL_ALL);
-
-    /* get a curl handle */
-    curl = curl_easy_init();
-    if(curl) {
-        /* First set the URL that is about to receive our POST. This URL can
-           just as well be a https:// URL if that is what should receive the
-           data. */
-        curl_easy_setopt(curl, CURLOPT_URL, "https://triangela.net/uat/parking/sync.php");
-        /* Now specify the POST data */
-        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, json_object_to_json_string(json_park));
-        /* Perform the request, res will get the return code */
-        res = curl_easy_perform(curl);
-        /* Check for errors */
-        if(res != CURLE_OK)
-            fprintf(stderr, "curl_easy_perform() failed: %s\n",
-                    curl_easy_strerror(res));
-
-        /* always cleanup */
-        curl_easy_cleanup(curl);
-    }
-    curl_global_cleanup();
-}
